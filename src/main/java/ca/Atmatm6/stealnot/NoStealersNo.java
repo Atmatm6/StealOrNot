35a1fc906f3c79f6febdcb760402867d2d91868d
Atmatm6 Yolo <atmatm6000@gmail.com>
2015-03-12 17:26:09 -0600

    SuP?

--- src/main/java/ca/Atmatm6/stealnot/NoStealersNo.java
+++ src/main/java/ca/Atmatm6/stealnot/NoStealersNo.java
@@ -1,27 +1,21 @@
 package ca.Atmatm6.stealnot;
 
-import ca.Atmatm6.stealnot.init.Blocks;
 import ca.Atmatm6.stealnot.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import static ca.Atmatm6.catalog.common.Catalog.stealog;
 
 @Mod(modid = Reference.Strings.MODID, version = Reference.Strings.VERSIUN, name = Reference.Strings.NAMECON)
 public class NoStealersNo{
-
-    public static Logger stealog = LogManager.getLogger(Reference.Strings.NAMECON);
-
     @EventHandler
     public void preInit(FMLPreInitializationEvent preInitializationEvent)
     {
         stealog.log(Level.INFO, Reference.Strings.STEALINGLOGIN + "Pre Initialization Mode.");
-        GameRegistry.registerBlock(Blocks.secureOre, "secure");
         stealog.log(Level.INFO, Reference.Strings.STEALINGLOGOUT + "Pre Initialization Mode");
     }
 
@@ -29,7 +23,6 @@ public class NoStealersNo{
     public void init(FMLInitializationEvent event)
     {
         stealog.log(Level.INFO, Reference.Strings.STEALINGLOGIN + "Initialization Mode.");
-
         stealog.log(Level.INFO, Reference.Strings.STEALINGLOGOUT + "Initialization Mode");
     }
 
@@ -37,7 +30,6 @@ public class NoStealersNo{
     public void postInit(FMLPostInitializationEvent event)
     {
         stealog.log(Level.INFO, Reference.Strings.STEALINGLOGIN + "Post Initialization Mode");
-
         stealog.log(Level.INFO, Reference.Strings.STEALINGLOGOUT + "Post Initialization Mode");
     }
 }
---
+++ src/main/java/ca/Atmatm6/stealnot/reference/Booleans.java
@@ -0,0 +1,7 @@
+package ca.Atmatm6.stealnot.reference;
+
+import cpw.mods.fml.common.Loader;
+
+public class Booleans {
+    public static final Boolean bedgistryInstalled = Loader.isModLoaded("bedmakerapi");
+}
